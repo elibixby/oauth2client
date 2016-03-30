596aef5b66a642e48ed3541a06beda57ba020ead
elibixby <elibixby@google.com>
2016-03-29 18:38:04 -0700

    Doc string fix

--- tests/contrib/test_iam_signer.py
+++ tests/contrib/test_iam_signer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for oauth2client.contrib.gce."""
+"""Unit tests for oauth2client.contrib.iam_signer."""
 
 from six.moves import http_client
 import unittest2
